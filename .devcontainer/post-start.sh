17c872273febd137a9ab7601ec06df28be99ea36
James Cape <jamescape777@pm.me>
2026-01-11 02:34:01 +0000

    ci: switch to nextest

--- .devcontainer/post-start.sh
+++ .devcontainer/post-start.sh
@@ -6,6 +6,7 @@ ln -sf /usr/local/cargo/bin /workspaces/iso4217/.cache/cargo/
 cargo binstall -q -y --force prek
 cargo binstall -q -y --force action-validator
 cargo binstall -q -y --force cargo-deny
+cargo binstall -q -y --force cargo-nextest
 cargo binstall -q -y --force cargo-llvm-cov
 cargo binstall -q -y --force cargo-semver-checks
 cargo binstall -q -y --force release-plz
