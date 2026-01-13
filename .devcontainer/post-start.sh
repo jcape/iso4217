0e6348d5fedec64cccb5dd2d88e90b4a8a15cac9
James Cape <jamescape777@pm.me>
2026-01-13 14:00:24 +0000

    ci: install nightly, stable in devcontainer

--- .devcontainer/post-start.sh
+++ .devcontainer/post-start.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+rustup toolchain install nightly
+rustup toolchain install stable
+
 mkdir -p /workspaces/iso4217/.cache/cargo
 ln -sf /usr/local/cargo/bin /workspaces/iso4217/.cache/cargo/
 
@@ -8,8 +11,6 @@ cargo binstall -q -y --force action-validator
 cargo binstall -q -y --force cargo-deny
 cargo binstall -q -y --force cargo-nextest
 cargo binstall -q -y --force cargo-llvm-cov
-cargo binstall -q -y --force cargo-semver-checks
-cargo binstall -q -y --force release-plz
 
 pushd /workspaces/iso4217 >/dev/null
 prek install -f >/dev/null
